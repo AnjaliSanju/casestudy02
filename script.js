1e5ccc0234db6bf5d54352932cd842062b9f2c4a
AnjaliSanju <anjalijayan129@gmail.com>
2024-07-29 13:39:42 +0530

    form validation

--- script.js
+++ script.js
@@ -1,54 +1,97 @@
-function validateInput() {
 
-     var fname =
-        document.getElementById("fname").value;
-    var lname =
-        document.getElementById("lname").value;
-    var email =
-        document.getElementById("email").value;
-    
-    var emailerror=document.getElementById("emailerror");
-        
-    const nameError =     document.getElementById("name-error");
-    // const passwordError = document.getElementById("password-error");
+const sform = document.getElementById('signupForm');
+const fname = document.getElementById('fname');
+const lname = document.getElementById('lname');
+// const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+// const phonenumber = document.getElementById('phonenumber');
 
-    
+sform.addEventListener('submit', e => {
+    e.preventDefault();
+    validateInputs();
+});
 
-    let isValid = true;
-    let regex=/^([A-Za-z0-9\.-]+)@([A-Za-z0-9\-]+).([a-z]{2,3})(.[a-z]{2,3})?$/;
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
-    // if (fname === "" || /\d/.test(fname)) {
-    //     nameError.textContent =
-    //         "Please enter your first name properly.";
-    //     isValid = false;
-    // }
-    if (!regex.test(email)||email =="") {
-        emailerror.innerText =
-            "Please enter a valid email address.";
-        isValid = false;
-    }
-
-    // if (password === "" || password.length < 6) {
-    //     passwordError.textContent =
-    //         "Please enter a password with at least 6 characters.";
-    //     isValid = false;
-    // }
-
-    return isValid;
-
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success')
 }
 
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
 
-// function validateInput(){
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
 
-//     var email=document.getElementById("email");
-//     var emailerror=document.getElementById("emailerror");
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());   
+}
 
-//     if(email.value.trim()==""){
-//         emailerror.innerText="Invalid";
-//         return false;
-//     }else{
-//         return true;
-//     }
+const isValidPhonenumber = phonenumber => {
+    var regx = /^[6-9]\d{9}$/ ;
+    return regx.test(phonenumber);   
+}
+const isValidPassword = password => {
+    var regx = /^(?=.*[0-9])[a-zA-Z0-9!@#$%^&*]{8,16}$/ ;
+    return regx.test(password);   
+}
 
-// }
+ 
+
+const validateInputs = () => {
+    const emailValue = email.value.trim();
+    const fnameValue=fname.value.trim();
+    const lnameValue=lname.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    const phonenumberValue=phonenumber.value.trim();
+
+    if(fnameValue === '') {
+        setError(fname, 'First name is required');
+    } else {
+        setSuccess(fname);
+    }
+    if(lnameValue === '') {
+        setError(lname, 'Last name is required');
+    } else {
+        setSuccess(lname);
+    }
+    if(emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+    if(phonenumberValue === '') {
+        setError(phonenumber, 'Phonenumber is required');
+    } else if (!isValidPhonenumber(phonenumberValue)) {
+        setError(phonenumber, 'Provide a valid phone number');
+    } else {
+        setSuccess(phonenumber);
+    }
+    if(passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (!isValidPassword(passwordValue)) {
+        setError(password, 'Please follow the instructions below');
+    } else {
+        setSuccess(password);
+    }
+    
+
+    if(password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+};
